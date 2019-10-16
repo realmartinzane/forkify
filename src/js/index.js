e351b1f3f6e99042ad81abda551dc0b845b7b8b9
Martin Zane <realmartinzane@gmail.com>
2019-10-16 14:14:53 +0200

    display loading spinner on search

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search'
 import * as searchView from './views/search'
-import { elements } from './views/base'
+import { elements, renderLoader, clearLoader } from './views/base'
 
 /* Global State
  * - Search Object
@@ -13,17 +13,18 @@ const state = {}
 const controlSearch = async () =>
 {
     const query = searchView.getInput();
-    console.log(query)
 
     if(query)
     {
         state.search = new Search(query)
 
-        searchView.clearResults();
-        searchView.clearInput();
+        searchView.clearResults()
+        searchView.clearInput()
+        renderLoader(elements.searchResults)
 
         await state.search.getResults()
 
+        clearLoader();
         searchView.renderResults(state.search.result)
     }
 }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,5 +2,30 @@ export const elements =
 {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
-    searchResultList: document.querySelector('.results__list')
+    searchResults: document.querySelector('.results'),
+    searchResultList: document.querySelector('.results__list'),
+}
+
+export const elementStrings = 
+{
+    loader: 'loader'
+}
+
+export const renderLoader = parent => 
+{
+    const loader = 
+    `
+    <div class="loader">
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>
+    `
+    parent.insertAdjacentHTML('afterbegin', loader)
+}
+
+export const clearLoader = () => 
+{
+    const loader = document.querySelector(`.${elementStrings.loader}`)
+    if(loader) loader.parentElement.removeChild(loader)
 }
