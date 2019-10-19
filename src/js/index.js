f44b0a50d8bd7b9b1274b231bb9e4cf79d0095b8
Martin Zane <realmartinzane@gmail.com>
2019-10-19 09:26:36 +0200

    parse ingredient count to the UI and add active class to current active recipe

--- src/js/index.js
+++ src/js/index.js
@@ -75,6 +75,8 @@ const controlRecipe = async () =>
         recipeView.clearRecipe()
         renderLoader(elements.recipe)
 
+        if(state.search) searchView.highlightSelected(id)
+
         state.recipe = new Recipe(id)
 
         try 
